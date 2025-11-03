e8fc387d7c91b83d564bbcdda6ce035799683890
tranthanhtra <thanhtra2001cm@gmail.com>
2025-11-03 18:26:48 +0700

    update script build

--- tcrm_desktop_installer.rb
+++ tcrm_desktop_installer.rb
@@ -1,7 +1,7 @@
 class TcrmDesktopInstaller < Formula
   desc "TCRM Flutter macOS app"
-  homepage "https://github.com/thnhtra2001/tcrm_desktop_installer"
-  url "https://github.com/thnhtra2001/tcrm_desktop.git"
+  homepage "https://github.com/thnhtra2001/tcrm_desktop/releases/download/v1.0.0/tcrm_desktop_installer.tar.gz"
+  url "https://github.com/thnhtra2001/tcrm_desktop/releases/download/v1.0.0/tcrm_desktop_installer.tar.gz"
   sha256 "90008c192c89e7029bdd5eeb9a1420f8c1a182cfbc66b6f39d0f838ce916dbed"
   version "1.0.0"
 
